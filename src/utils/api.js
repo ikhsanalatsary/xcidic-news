57d29694795e0fb259fbb71345702e86f0f96863
Abdul Fattah Ikhsan <ikhsannetwork@gmail.com>
2018-04-06 20:08:33 +0700

    refactor api, add cancellation, secret apikey to env & add base favicon url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,41 +1,43 @@
 import axios from 'axios';
 import qs from 'querystringify';
-import { BASE_API_URL, API_KEY } from './config'
+import { BASE_API_URL } from './config';
 
-function create (baseUr = BASE_API_URL, options = {}) {
+const CancelToken = axios.CancelToken;
+
+function create (baseURL = BASE_API_URL, options = {}) {
   const api = axios.create({
-    baseURL: baseUr,
+    baseURL,
     headers: {
-      'X-Api-Key': API_KEY,
+      'X-Api-Key': process.env.REACT_APP_NEWS_API_KEY,
     },
     // 10 second timeout...
     timeout: 10000,
     ...options
   });
 
-  const getTopHeadline = (params, headers = {}) => {
+  const getTopHeadline = (params, config = {}) => {
     const query = qs.stringify(params)
     const endpoint = '/v2/top-headlines'
     let uri = query ? `${endpoint}?${query}` : endpoint
-    return api.get(uri, null, { headers })
+    return api.get(uri, config)
   }
-  const getEverything = (params, headers = {}) => {
+  const getEverything = (params, config = {}) => {
     const query = qs.stringify(params)
     const endpoint = '/v2/everything'
     let uri = query ? `${endpoint}?${query}` : endpoint
-    return api.get(uri, null, { headers })
+    return api.get(uri, config)
   }
-  const getArticles = (params, headers = {}) => {
+  const getArticles = (params, config = {}) => {
     const query = qs.stringify(params)
     const endpoint = '/v1/articles'
     let uri = query ? `${endpoint}?${query}` : endpoint
-    return api.get(uri, null, { headers })
+    return api.get(uri, config)
   }
-  const getSources = (params, headers = {}) => {
+  const getSources = (params, config = {}) => {
     const query = qs.stringify(params)
     const endpoint = '/v2/sources'
     let uri = query ? `${endpoint}?${query}` : endpoint
-    return api.get(uri, null, { headers })
+    return api.get(uri, config)
   }
 
   return {
@@ -47,5 +49,6 @@ function create (baseUr = BASE_API_URL, options = {}) {
 }
 
 export default {
-  create
+  create,
+  CancelToken
 };
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
 export const BASE_API_URL = 'https://newsapi.org';
-export const API_KEY = 'caa9c0fca7bf4fa59539b9cc6388067a';
+export const BASE_ICON_URL = 'https://besticon-demo.herokuapp.com/icon'; //?url=http://us.cnn.com&size=70..120..200
