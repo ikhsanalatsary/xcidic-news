7ccd65b76d84c64b453624280f007f989c07b13c
Abdul Fattah Ikhsan <skull.saders18@gmail.com>
2018-07-15 19:40:52 +0700

    add search info

--- src/modules/news/Search.js
+++ src/modules/news/Search.js
@@ -1,172 +1,207 @@
-import React from 'react';
-import { Card, Dimmer, Image, Loader, Pagination, Responsive, Segment } from 'semantic-ui-react'
-import qs from 'querystringify';
-import API from '../../utils/api'
-import NewsItem from './components/NewsItem'
-import paragraph from './paragraph.png'
+import React from "react";
+import {
+  Card,
+  Dimmer,
+  Image,
+  Loader,
+  Pagination,
+  Responsive,
+  Segment,
+  Message
+} from "semantic-ui-react";
+import qs from "querystringify";
+import API from "../../utils/api";
+import NewsItem from "./components/NewsItem";
+import paragraph from "./paragraph.png";
 
 const api = API.create();
 const CancelToken = API.CancelToken;
 
 class Search extends React.PureComponent {
   static getDerivedStateFromProps(nextProps, prevState) {
-    return{
+    return {
       q: qs.parse(nextProps.location.search).q
-    }
+    };
   }
 
-  cancel = null
+  cancel = null;
   state = {
     loading: true,
     count: 0,
     data: [],
-    error: '',
+    error: "",
     activePage: 1,
     fetching: false,
     currentPage: 1,
     dimmed: false,
     q: qs.parse(this.props.location.search).q
-  }
+  };
 
   componentDidMount() {
-    this.fetchData()
-    window.addEventListener('scroll', this.onScroll, false)
+    this.fetchData();
+    window.addEventListener("scroll", this.onScroll, false);
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.q !== this.state.q) {
-      this.setState({
-        loading: true
-      }, this.fetchData)
+      this.setState(
+        {
+          loading: true
+        },
+        this.fetchData
+      );
     }
   }
 
   componentWillUnmount() {
     if (this.cancel) {
-      this.cancel('Operation canceled by the user.')
+      this.cancel("Operation canceled by the user.");
     }
-    window.removeEventListener('scroll', this.onScroll, false)
+    window.removeEventListener("scroll", this.onScroll, false);
   }
 
   onScroll = async () => {
     if (
       window.screen.width < 768 &&
-      (window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 500) &&
+      window.innerHeight + window.scrollY >= document.body.offsetHeight - 500 &&
       (this.state.data.length && this.state.currentPage !== this.state.count) &&
       !this.state.fetching
     ) {
-      let nextPage = this.state.currentPage + 1
+      let nextPage = this.state.currentPage + 1;
       this.setState({
-          currentPage: nextPage,
-          fetching: true,
-        })
+        currentPage: nextPage,
+        fetching: true
+      });
 
-        let page
-        if (this.state.currentPage === 1) {
-          page = 2
-          this.setState({ currentPage: 2, fetching: true })
-        } else {
-          page = nextPage
-        }
-        try {
-          let data = [...this.state.data]
-          const result = await api.getEverything({
+      let page;
+      if (this.state.currentPage === 1) {
+        page = 2;
+        this.setState({ currentPage: 2, fetching: true });
+      } else {
+        page = nextPage;
+      }
+      try {
+        let data = [...this.state.data];
+        const result = await api.getEverything(
+          {
             q: this.state.q,
-            sortBy: 'publishedAt',
-            page,
-          }, {
+            sortBy: "publishedAt",
+            page
+          },
+          {
             cancelToken: new CancelToken(c => {
               // An executor function receives a cancel function as a parameter
               this.cancel = c;
             })
-          })
-          if (result && result.data) {
-            result.data.articles.forEach((item, index) => data.push(item))
-            this.setState({
-              data,
-              fetching: false
-            })
           }
-        } catch (e) {
-          console.log('e', e);
+        );
+        if (result && result.data) {
+          result.data.articles.forEach((item, index) => data.push(item));
+          this.setState({
+            data,
+            fetching: false
+          });
         }
+      } catch (e) {
+        console.log("e", e);
+      }
     }
-  }
+  };
 
-  articleList = (article, i) => <NewsItem article={article} key={i} />
+  articleList = (article, i) => <NewsItem article={article} key={i} />;
 
   fetchData = (page = 1) => {
-    api.getEverything({
-      q: this.state.q,
-      sortBy: 'publishedAt',
-      page,
-    }, {
-      cancelToken: new CancelToken(c => {
-        // An executor function receives a cancel function as a parameter
-        this.cancel = c;
+    api
+      .getEverything(
+        {
+          q: this.state.q,
+          sortBy: "publishedAt",
+          page
+        },
+        {
+          cancelToken: new CancelToken(c => {
+            // An executor function receives a cancel function as a parameter
+            this.cancel = c;
+          })
+        }
+      )
+      .then(result => {
+        this.setState({
+          data: result.data.articles,
+          count: result.data.totalResults,
+          loading: false,
+          dimmed: false
+        });
       })
-    })
-    .then(result => {
-      this.setState({
-        data: result.data.articles,
-        count: result.data.totalResults,
-        loading: false,
-        dimmed: false,
+      .catch(e => {
+        this.setState({
+          loading: false,
+          dimmed: false,
+          error: e.message
+        });
       });
-    })
-    .catch(e => {
-      this.setState({
-        loading: false,
-        dimmed: false,
-        error: e.message
-      })
-    })
-  }
+  };
 
   handlePaginationChange = (e, { activePage }) => {
-    this.setState(({ dimmed }) => ({ activePage, dimmed: !dimmed }),
+    this.setState(
+      ({ dimmed }) => ({ activePage, dimmed: !dimmed }),
       () => this.fetchData(activePage)
-    )
-  }
+    );
+  };
 
   render() {
-    const { loading, error, data, count, activePage, fetching, dimmed } = this.state;
+    const {
+      loading,
+      error,
+      data,
+      count,
+      activePage,
+      fetching,
+      dimmed,
+      q
+    } = this.state;
     let content = null;
     if (loading) {
       content = (
         <Segment>
-           <Dimmer active inverted>
-             <Loader inverted>Loading</Loader>
-           </Dimmer>
-           <p>
-             <Image src={paragraph} />
-           </p>
-           <p>
-             <Image src={paragraph} />
-           </p>
-           <p>
-             <Image src={paragraph} />
-           </p>
-         </Segment>
-      )
+          <Dimmer active inverted>
+            <Loader inverted>Loading</Loader>
+          </Dimmer>
+          <p>
+            <Image src={paragraph} />
+          </p>
+          <p>
+            <Image src={paragraph} />
+          </p>
+          <p>
+            <Image src={paragraph} />
+          </p>
+        </Segment>
+      );
     } else if (data && data.length > 0) {
       content = (
-        <Dimmer.Dimmable as={Card.Group} dimmed={dimmed} stackable itemsPerRow={3} style={{ marginBottom: 15 }}>
+        <Dimmer.Dimmable
+          as={Card.Group}
+          dimmed={dimmed}
+          stackable
+          itemsPerRow={3}
+          style={{ marginBottom: 15 }}
+        >
           <Dimmer active={dimmed} inverted>
             <Loader>Loading</Loader>
           </Dimmer>
           {data.map(this.articleList)}
         </Dimmer.Dimmable>
-      )
+      );
     } else {
-      content = (
-        <Segment>
-          {error}
-        </Segment>
-      )
+      content = <Segment>{error}</Segment>;
     }
     return (
       <React.Fragment>
+        <Message>
+          <Message.Header>You search for:</Message.Header>
+          <p style={{ fontStyle: "italic" }}>{q}</p>
+        </Message>
         {content}
         <Responsive
           as={Pagination}
@@ -174,9 +209,9 @@ class Search extends React.PureComponent {
           onPageChange={this.handlePaginationChange}
           totalPages={count}
           minWidth={768}
-          style={{marginBottom: 15}}
+          style={{ marginBottom: 15 }}
         />
-        {fetching && <Loader active inline='centered' />}
+        {fetching && <Loader active inline="centered" />}
       </React.Fragment>
     );
   }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
-import qs from 'querystringify';
-import { BASE_API_URL } from './config';
+import axios from "axios";
+import qs from "querystringify";
+import { BASE_API_URL } from "./config";
 
 const CancelToken = axios.CancelToken;
 
-function create (baseURL = BASE_API_URL, options = {}) {
+function create(baseURL = BASE_API_URL, options = {}) {
   const api = axios.create({
     baseURL,
     headers: {
-      'X-Api-Key': process.env.REACT_APP_NEWS_API_KEY,
+      "X-Api-Key": process.env.REACT_APP_NEWS_API_KEY
     },
     // 10 second timeout...
     timeout: 10000,
@@ -16,29 +16,29 @@ function create (baseURL = BASE_API_URL, options = {}) {
   });
 
   const getTopHeadline = (params, config = {}) => {
-    const query = qs.stringify(params)
-    const endpoint = '/v2/top-headlines'
-    let uri = query ? `${endpoint}?${query}` : endpoint
-    return api.get(uri, config)
-  }
+    const query = qs.stringify(params);
+    const endpoint = "/v2/top-headlines";
+    let uri = query ? `${endpoint}?${query}` : endpoint;
+    return api.get(uri, config);
+  };
   const getEverything = (params, config = {}) => {
-    const query = qs.stringify(params)
-    const endpoint = '/v2/everything'
-    let uri = query ? `${endpoint}?${query}` : endpoint
-    return api.get(uri, config)
-  }
+    const query = qs.stringify(params);
+    const endpoint = "/v2/everything";
+    let uri = query ? `${endpoint}?${query}` : endpoint;
+    return api.get(uri, config);
+  };
   const getArticles = (params, config = {}) => {
-    const query = qs.stringify(params)
-    const endpoint = '/v1/articles'
-    let uri = query ? `${endpoint}?${query}` : endpoint
-    return api.get(uri, config)
-  }
+    const query = qs.stringify(params);
+    const endpoint = "/v1/articles";
+    let uri = query ? `${endpoint}?${query}` : endpoint;
+    return api.get(uri, config);
+  };
   const getSources = (params, config = {}) => {
-    const query = qs.stringify(params)
-    const endpoint = '/v2/sources'
-    let uri = query ? `${endpoint}?${query}` : endpoint
-    return api.get(uri, config)
-  }
+    const query = qs.stringify(params);
+    const endpoint = "/v2/sources";
+    let uri = query ? `${endpoint}?${query}` : endpoint;
+    return api.get(uri, config);
+  };
 
   return {
     getTopHeadline,
